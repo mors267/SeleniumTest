43497fbc71191fe030a12dac3b677791bf20b6c8
mors267 <morsalin_abedin@comcast.com>
2024-09-06 19:34:26 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -1,37 +1,27 @@
+import time
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="module")
-def browser():
-    options = Options()
-    options.add_argument("--disable-notifications")
-    driver = webdriver.Chrome(options=options)
-    yield driver
-    driver.quit()
+class TestLoginPage:
+    def test_valid_login(self, driver):
+        driver.get("https://www.saucedemo.com/")
+        # time.sleep(2)
 
-def test_HomepageLOBTiles(browser):
-    # Load the URL
-    browser.get('https://business.comcast.com/?disablescripts=true')
-    browser.maximize_window()
-    browser.refresh()
+        # Type username
+        username_input = driver.find_element(By.ID, "user-name")
+        username_input.send_keys("standard_user")
 
-    # Define elements and their expected href values
-    elements = {
-        "//a[@href='/learn/mobile']": 'https://business.comcast.com/learn/mobile',
-        "//a[@href='/learn/internet']": 'https://business.comcast.com/learn/internet',
-        "//a[@href='/learn/phone']": 'https://business.comcast.com/learn/phone',
-        "//a[@href='/learn/internet/security-edge']": 'https://business.comcast.com/learn/internet/security-edge'
-    }
+        # Type password
+        password_input = driver.find_element(By.ID, "password")
+        password_input.send_keys("secret_sauce")
 
-    # Loop through elements, assert href value, and print result
-    for xpath, expected_href in elements.items():
-        element = browser.find_element(By.XPATH, xpath)
-        assert element.get_attribute('href') == expected_href
-        print('\n' + xpath + ' href value matches expected URL')
-        print("CTA URL:", element.get_attribute('href'))
-        
+        # Click on the login button
+        login_btn = driver.find_element(By.ID, "login-button")
+        login_btn.click()
 
+        # URL Validation
+        actual_url = driver.current_url
+        assert actual_url == "https://www.saucedemo.com/inventory.htmls"
 
+        # time.sleep(2)
