9954cf089364ec3ea6fb727adb61f238e6cc7ad1
mors267 <morsalin_abedin@comcast.com>
2024-09-06 18:52:06 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -1,9 +1,37 @@
-import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
 
-@pytest.mark.usefixtures("setup")
-class SampleTestClass:
-    def test_google_title(self):
-        self.driver.get('https://google.com')
-        title = self.driver.title
-        print(f'Page title: {title}')
-        assert title == 'google'
+chromedriver_autoinstaller.install()  
+chrome_options = webdriver.ChromeOptions()    
+
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
+
+driver.get('http://github.com')
+print(driver.title)
+with open('./GitHub_Action_Results.txt', 'w') as f:
+    f.write(f"This was written with a GitHub action {driver.title}")
