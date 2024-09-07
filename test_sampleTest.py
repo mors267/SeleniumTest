347026f2dc4f049519aae2a7b0339d998b29492a
mors267 <morsalin_abedin@comcast.com>
2024-09-06 20:19:08 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -36,5 +36,18 @@ driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('http://www.comcastbusiness.com')
 print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+
+# Define elements and their expected href values
+    elements = {
+        "//a[@href='/learn/mobile']": 'https://business.comcast.com/learn/mobile',
+        "//a[@href='/learn/internet']": 'https://business.comcast.com/learn/internet',
+        "//a[@href='/learn/phone']": 'https://business.comcast.com/learn/phone',
+        "//a[@href='/learn/internet/security-edge']": 'https://business.comcast.com/learn/internet/security-edge'
+    }
+
+    # Loop through elements, assert href value, and print result
+    for xpath, expected_href in elements.items():
+        element = browser.find_element(By.XPATH, xpath)
+        assert element.get_attribute('href') == expected_href
+        print('\n' + xpath + ' href value matches expected URL')
+        print("CTA URL:", element.get_attribute('href'))
