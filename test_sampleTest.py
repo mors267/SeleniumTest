6800b2ce8bf73aac5a730c3cf847a09892b35a11
mors267 <morsalin_abedin@comcast.com>
2024-09-06 22:36:51 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -7,31 +7,32 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
+# import chromedriver_autoinstaller
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 800))  
+# display.start()
 
-chrome_options = webdriver.ChromeOptions()    
-# Add your options as needed    
-options = [
-  # Define window size here
-   "--window-size=1920,1080",
-    "--ignore-certificate-errors"
+# chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+#                                       # and if it doesn't exist, download it automatically,
+#                                       # then add chromedriver to path
+
+# chrome_options = webdriver.ChromeOptions()    
+# # Add your options as needed    
+# options = [
+#   # Define window size here
+#    "--window-size=1920,1080",
+#     "--ignore-certificate-errors"
  
-    #"--headless",
-    #"--disable-gpu",
-    #"--window-size=1920,1200",
-    #"--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    #"--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
-]
+#     #"--headless",
+#     #"--disable-gpu",
+#     #"--window-size=1920,1200",
+#     #"--ignore-certificate-errors",
+#     #"--disable-extensions",
+#     #"--no-sandbox",
+#     #"--disable-dev-shm-usage",
+#     #'--remote-debugging-port=9222'
+# ]
 
 
 @pytest.fixture()    
