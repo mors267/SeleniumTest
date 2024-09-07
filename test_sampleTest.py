9ccab8a5e7e6091b5ab3c8ae306ac994496fe1c3
mors267 <morsalin_abedin@comcast.com>
2024-09-06 22:25:48 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -30,201 +30,50 @@ options = [
     #'--remote-debugging-port=9222'
 ]
 
+
 @pytest.fixture()    
 def quicksetup(): 
     global driver
     options = Options()
     options.add_argument("--disable-notifications")
     driver = webdriver.Chrome()
-    driver.get('https://business.comcast.com/learn/internet/sd-wan-small-business/?disablescripts=true')
+    driver.get('https://business.comcast.com/shop/gateway?disablescripts=true')
     driver.maximize_window()
     driver.refresh()
     yield driver
-    driver.quit()
+    driver.quit() 
 
-def test_ReqAQuoteButtonHero(quicksetup):
-
-    driver = quicksetup
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div/div[4]/a').get_attribute('title')
-    assert element == 'REQUEST A QUOTE'
+def test_OOF(quicksetup):
     
-    #validate correct URl is present
-    if "REQUEST A QUOTE" in element: 
-        print('Request a Quote CTA available on hero') 
-    else: 
-        print('Request a Quote CTA NOT available on hero')  
+    driver = quicksetup  
 
-
-def test_Features(quicksetup):
-
-    driver = quicksetup
-    
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[1]/a').click()
-    time.sleep(2)
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[14]/div[1]/div/div/h2').text
-    assert element == 'Secure SD-WAN. Simplified.'
-
-    if "Secure SD-WAN. Simplified." in element: 
-        print('Features section available on page') 
-    else: 
-        print('Features section NOT available on page')
-
-def test_NetworkManagement(quicksetup):
-
-    driver = quicksetup
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[2]/a').click()
-    time.sleep(2)
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="network management"]/div/div/div[1]/div/h3').text
-    assert element == 'Manage and monitor your network in one centralized digital portal'
-
-    if "Manage and monitor your network in one centralized digital portal" in element: 
-        print('Network Management section available on page') 
-    else: 
-        print('Network Management NOT available on page')
+    timeout = 15
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except TimeoutException:
+        print ('Gateway page did not display')
         
-def test_Security(quicksetup):
-
-    driver = quicksetup
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[3]/a').click()
-    time.sleep(2)
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="security"]/div/div/div[2]/div/h3').text
-    assert element == 'Security for peace of mind'
-
-    if "Security for peace of mind" in element: 
-        print('Security section available on page') 
+    driver.find_element(by=By.XPATH, value="//input[@id='address']").click()
+    driver.find_element(By.ID, "address").send_keys("211 W 56TH ST FRNT 1 NEW YORK NY 10019")
+    time.sleep(5)
+    driver.find_element(by=By.XPATH, value='//*[@id="0"]').click()
+    driver.find_element(by=By.XPATH, value="//button[@type='submit']").click()
+    
+    timeout = 15
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='FIND A DIFFERENT PROVIDER']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        print ('Could not localize OOF address location!')
+    time.sleep(3)
+    
+    element = driver.find_element(by=By.XPATH, value="//h2[@class='headline-4']").text
+    assert element == 'Our service isn’t available at this address. You can try a different address or explore other options.'
+    
+    #validate correct text is present
+    if "Our service isn’t available at this address. You can try a different address or explore other options." in element: 
+        print('Out of Footprint messaging displaying correctly') 
     else: 
-        print('Security NOT available on page')
+        print('Out of Footprint messaging not displaying correctly')  
         
-    # learn more CTA
-        
-    learn_more_locator = '//*[@id="security"]/div/div/div[2]/div/a'
-    learn_more_elm = driver.find_element(By.XPATH, learn_more_locator)
-    learn_more_elm.click()
-    
-    cur_url = driver.current_url
-    expected_url = 'https://business.comcast.com/enterprise/products-services/cybersecurity-services/advanced-security'
-    
-    assert cur_url == expected_url, f"Expected URL: {expected_url}, but got: {driver.current_url}"
-    print("Learn More link redirects to the correct URL.")
-    
-def test_SmallBusinessSolution(quicksetup):
-
-    driver = quicksetup
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[4]/a').click()
-    time.sleep(2)
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="solution"]/div[1]/div/div/h2').text
-    assert element == 'The network solution your small business needs'
-
-    if "The network solution your small business needs" in element: 
-        print('Small business solution section available on page') 
-    else: 
-        print('Small business solution section NOT available on page')
-        
-    # connect with an agent link
-        
-    connect_agent = '//*[@id="solution"]/div[2]/table/tbody/tr[6]/td/ul/li/a'
-    connect_agent_elm = driver.find_element(By.XPATH, connect_agent)
-    connect_agent_elm.click()
-    
-    cur_url = driver.current_url
-    expected_url = 'https://business.comcast.com/contact'
-    
-    assert cur_url == expected_url, f"Expected URL: {expected_url}, but got: {driver.current_url}"
-    print("Connect with an agent link redirects to the correct URL.")
-
-def test_Insights(quicksetup):
-    driver = quicksetup
-
-    # Click the "Insights" section
-    driver.find_element(By.XPATH, '//*[@id="main"]/section/div[2]/div/ul/li[5]/a').click()
-    time.sleep(2)
-
-    # Verify the header in the "Insights" section
-    element = driver.find_element(By.XPATH, '//*[@id="insights"]/div[1]/div/div/h2').text
-    assert element == 'Industry insights', f"Expected 'Industry insights', but got '{element}'"
-
-    if "Industry insights" in element:
-        print('Industry insights section available on page')
-    else:
-        print('Industry insights section NOT available on page')
-
-    # First article redirect
-    first_article = '//*[@id="insights"]/div[2]/div[1]/div/a'
-    driver.find_element(By.XPATH, first_article).click()
-
-    # Switch to the new tab
-    window_handles = driver.window_handles
-    driver.switch_to.window(window_handles[-1])
-
-    cur_url = driver.current_url
-    expected_url = 'https://business.comcast.com/community/browse-all/details/from-single-location-businesses-to-enterprise-branch-of-one-how-sd-wan-is-evolving-to-address-more-needs'
-
-    assert cur_url == expected_url, f"Expected URL: {expected_url}, but got: {cur_url}"
-    print("First article link redirects to the correct URL.")
-
-    # Close the current tab and switch back to the original
-    driver.close()
-    driver.switch_to.window(window_handles[0])
-    time.sleep(2)
-
-    # Second article redirect
-    second_article = '//*[@id="insights"]/div[2]/div[2]/div/a'
-    driver.find_element(By.XPATH, second_article).click()
-
-    # Switch to the new tab
-    window_handles = driver.window_handles
-    driver.switch_to.window(window_handles[-1])
-
-    cur_url = driver.current_url
-    expected_url = 'https://business.comcast.com/community/browse-all/details/sd-wan-for-small-midsize-and-franchise-businesses-standalone-and-cloud-first-solutions'
-
-    assert cur_url == expected_url, f"Expected URL: {expected_url}, but got: {cur_url}"
-    print("Second article link redirects to the correct URL.")
-
-    # Close the current tab and switch back to the original
-    driver.close()
-    driver.switch_to.window(window_handles[0])
-    time.sleep(2)
-
-    # Third article redirect
-    third_article = '//*[@id="insights"]/div[2]/div[3]/div/a'
-    driver.find_element(By.XPATH, third_article).click()
-
-    # Switch to the new tab
-    window_handles = driver.window_handles
-    driver.switch_to.window(window_handles[-1])
-
-    cur_url = driver.current_url
-    expected_url = 'https://business.comcast.com/community/browse-all/details/use-sd-wan-if-you-re-doing-any-of-these-five-things'
-
-    assert cur_url == expected_url, f"Expected URL: {expected_url}, but got: {cur_url}"
-    print("Third article link redirects to the correct URL.")
-
-    # Close the current tab and switch back to the original
-    driver.close()
-    driver.switch_to.window(window_handles[0])
-    time.sleep(2)
-
-                             
-def test_ReqAQuoteJumpLink(quicksetup):                                                                                     
-                                                 
-    driver = quicksetup
-    
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[6]/a').click()       
-    time.sleep(2)
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="lead-form-container"]/div/legend').text
-    assert element == 'Request a sales consultation'
-              
-    if "Request a sales consultation" in element: 
-        print('Lead Form section available on page') 
-    else: 
-        print('Lead Form section NOT available on page') 
