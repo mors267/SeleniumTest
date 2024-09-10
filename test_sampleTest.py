a580b00f7cd6694c952492272d9a427032ffc049
mors267 <morsalin_abedin@comcast.com>
2024-09-10 12:42:35 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -23,13 +23,7 @@ options = [
     "--window-size=1920,1200",
     "--ignore-certificate-errors"
     "--headless",
-    "--disable-gpu",
-    #"--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    #"--disable-extensions",
-    #"--no-sandbox",
-    "--disable-dev-shm-usage",
-    #'--remote-debugging-port=9222'
+    "--disable-gpu"
 ]
 
 for option in options:
@@ -37,19 +31,18 @@ for option in options:
 
 
 @pytest.fixture(scope="module")    
-def driver(): 
+def driver():
     options = Options()
     options.add_argument("--disable-notifications")
     options.add_argument("--headless")
     options.add_argument("--window-size=1920, 1200")
     driver = webdriver.Chrome(options=options)
     yield driver
-    driver.quit()  
+    driver.quit() 
 
 def test_ShopNowButtonHero(driver):
     driver.get('https://business.comcast.com/learn/internet?disablescripts=true')
     driver.maximize_window()
-    driver.refresh()
   
     #view all offers link
   
@@ -61,9 +54,6 @@ def test_ShopNowButtonHero(driver):
 def test_SeeItInAction(driver):
     driver.get('https://business.comcast.com/learn/internet?disablescripts=true')
     driver.maximize_window()
-    driver.refresh()
-  
-    driver.get('https://business.comcast.com/learn/internet?disablescripts=true')
 
     driver.find_element(by=By.XPATH, value="//span[contains(text(),'SEE IT IN ACTION')]").click()
     time.sleep(2)
@@ -77,61 +67,3 @@ def test_SeeItInAction(driver):
     else: 
         print('Video link NOT available on homepage hero')  
 
-def test_JumpLinks(driver):  
-
-    driver.get('https://business.comcast.com/learn/internet?disablescripts=true')
-    driver.maximize_window()
-    driver.refresh()
-
-#explore speeds jump link
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[1]/a').click()
-    time.sleep(1)
-
-    element = driver.find_element(by=By.XPATH, value="//h2[normalize-space()='BUSINESS INTERNET STANDARD']").text
-    assert element == 'BUSINESS INTERNET STANDARD'
-
-    if "BUSINESS INTERNET STANDARD" in element: 
-        print('Explore Speeds section available from jump link') 
-    else: 
-        print('Explore Speeds jump link not working') 
-
-#solutions jump link
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[2]/a').click()
-    time.sleep(1)
-
-    element = driver.find_element(by=By.XPATH, value="//h3[normalize-space()='Helping you stay connected plus protected']").text
-    assert element == 'Helping you stay connected plus protected'
-
-    if "Helping you stay connected plus protected" in element: 
-        print('Solutions section available from jump link') 
-    else: 
-        print('Solutions jump link not working') 
-
-#get a quote jump link
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[3]/a').click()
-    time.sleep(1)
-
-    element = driver.find_element(by=By.XPATH, value="//legend[@class='headline-4']").text
-    assert element == 'Get a free quote in 2 easy steps'
-
-    if "Get a free quote in 2 easy steps" in element: 
-        print('Get a quote section available from jump link') 
-    else: 
-        print('Get a quote jump link not working') 
-
-
-#get pricing jump link  
-
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/ul/li[4]/a').click()
-    time.sleep(1)
-
-    element = driver.find_element(by=By.XPATH, value="//h2[contains(text(),'Let’s find some deals in your area')]").text
-    assert element == 'Let’s find some deals in your area'
-
-    if "Let’s find some deals in your area" in element: 
-        print('Get pricing section available from jump link') 
-    else: 
-        print('Get pricing jump link not working') 
