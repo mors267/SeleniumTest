63ffff4d65e93ec026f3d0e326a49f3f0fd755d3
sadiaorpi <100485787+sadiaorpi@users.noreply.github.com>
2024-09-10 14:08:40 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -81,38 +81,3 @@ def test_ReqAQuoteLink(driver):
     else: 
         print('Lead Gen NOT available on page') 
 
-def test_OOF(driver):
-
-    driver.get('https://business.comcast.com/shop/gateway?disablescripts=true')
-    driver.maximize_window()
-
-    timeout = 15
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        WebDriverWait(driver, timeout).until(element_present)
-    except TimeoutException:
-        print ('Gateway page did not display')
-        
-    driver.find_element(by=By.XPATH, value="//input[@id='address']").click()
-    driver.find_element(By.ID, "address").send_keys("211 W 56TH ST FRNT 1 NEW YORK NY 10019")
-    time.sleep(5)
-    driver.find_element(by=By.XPATH, value='//*[@id="0"]').click()
-    driver.find_element(by=By.XPATH, value="//button[@type='submit']").click()
-    
-    timeout = 15
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='FIND A DIFFERENT PROVIDER']"))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Could not localize OOF address location!')
-    time.sleep(3)
-    
-    element = driver.find_element(by=By.XPATH, value="//h2[@class='headline-4']").text
-    assert element == 'Our service isn’t available at this address. You can try a different address or explore other options.'
-    
-    #validate correct text is present
-    if "Our service isn’t available at this address. You can try a different address or explore other options." in element: 
-        print('Out of Footprint messaging displaying correctly') 
-    else: 
-        print('Out of Footprint messaging not displaying correctly')  
-
