a3c17ecbaf3229cf3a8b372451a5f1c64fbe2bba
mors267 <morsalin_abedin@comcast.com>
2024-09-12 10:19:04 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -38,161 +38,22 @@ def driver():
     yield driver
     driver.quit() 
 
-def test_BroadbandLabelNED(driver):  
-  
+def test_HomepageLOBTiles(driver):
+
+    driver.get('https://business.comcast.com/?disablescripts=true')
     driver.maximize_window()
 
-    driver.get('https://business.comcast.com/shop/gateway?disablescripts=true')
-    timeout = 15
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        WebDriverWait(driver, timeout).until(element_present)
-    except TimeoutException:
-        print ('Gateway page did not display')
-    driver.find_element(by=By.XPATH, value="//input[@id='address']").click()
-    driver.find_element(By.ID, "address").send_keys("4519 main st, manchester center, VT 05255")
-    time.sleep(5)
-    driver.find_element(by=By.XPATH, value='//*[@id="0"]').click()
-    driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/section/div/div/div[1]/div/div/form/div[2]/button').click()
+    # Define elements and their expected href values
+    elements = {
+        "//a[@href='/learn/mobile']": 'https://business.comcast.com/learn/mobile',
+        "//a[@href='/learn/internet']": 'https://business.comcast.com/learn/internet',
+        "//a[@href='/learn/phone']": 'https://business.comcast.com/learn/phone',
+        "//a[@href='/learn/internet/security-edge']": 'https://business.comcast.com/learn/internet/security-edge'
+    }
 
-    timeout = 15
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, "//p[normalize-space()='300 Mbps']"))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Shop offers page did not display!')
-    time.sleep(3)
-
-    # check broadband label on speed tiers 
-
-    #Essential
-    driver.get('https://business.comcast.com/shop/offers/?disablescripts=true')
-    time.sleep(.5)
-    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Essential']").click()
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="tabpanel-1"]/div/div[2]/div/div[1]')
-    driver.execute_script("return arguments[0].scrollIntoView();", element)
-    time.sleep(1.5)
-
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="broadband-label-BI150"]/div[1]/p[1]'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Labels section not showing')
-
-    element = driver.find_element(by=By.XPATH, value="//p[normalize-space()='Essential, 150 Mbps/25 Mbps']").text
-    assert element == 'Essential, 150 Mbps/25 Mbps'
-
-    if "Essential, 150 Mbps/25 Mbps" in element: 
-        print('Essential ingredient label displaying') 
-    else: 
-        print('Essential ingredient label NOT displaying')
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="broadband-label-BI150"]/div[1]/p[1]').text
-    print("Speed displayed in chart: " + element)
-
-    #Standard
-    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Standard']").click()
-
-    # element = driver.find_element(by=By.XPATH, value='//*[@id="tabpanel-2"]/div/div[2]/div/div[1]')
-    # driver.execute_script("return arguments[0].scrollIntoView();", element)
-
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="broadband-label-BI300"]/div[1]/p[1]'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Labels section not showing')
-
-
-    element = driver.find_element(by=By.XPATH, value="//p[normalize-space()='Standard, 300 Mbps/35 Mbps']").text
-    assert element == 'Standard, 300 Mbps/35 Mbps'
-
-    if "Standard, 300 Mbps/35 Mbps" in element: 
-        print('\n''Standard ingredient label displaying') 
-    else: 
-        print('\n''Standard ingredient label NOT displaying')
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="broadband-label-BI300"]/div[1]/p[1]').text
-    print("Speed displayed in chart: " + element)
-
-    #Performance
-    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Performance']").click()
-
-    # element = driver.find_element(by=By.XPATH, value='//*[@id="tabpanel-3"]/div/div[2]/div/div[1]')
-    # driver.execute_script("return arguments[0].scrollIntoView();", element)
-
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="broadband-label-BI500"]/div[1]/p[1]'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Labels section not showing')
-
-    element = driver.find_element(by=By.XPATH, value="//p[normalize-space()='Performance, 500 Mbps/35 Mbps']").text
-    assert element == 'Performance, 500 Mbps/35 Mbps'
-
-    if "Performance, 500 Mbps/35 Mbps" in element: 
-        print('\n''Performance ingredient label displaying') 
-    else: 
-        print('\n''Performance ingredient label NOT displaying')
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="broadband-label-BI500"]/div[1]/p[1]').text
-    print("Speed displayed in chart: " + element)
-
-    #Advanced
-    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Advanced']").click()
-
-    # element = driver.find_element(by=By.XPATH, value='//*[@id="tabpanel-4"]/div/div[2]/div/div[1]')
-    # driver.execute_script("return arguments[0].scrollIntoView();", element)
-
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="broadband-label-BI800"]/div[1]/p[1]'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Labels section not showing')
-
-    element = driver.find_element(by=By.XPATH, value="//p[normalize-space()='Advanced, 800 Mbps/35 Mbps']").text
-    assert element == 'Advanced, 800 Mbps/35 Mbps'
-
-    if "Advanced, 800 Mbps/35 Mbps" in element: 
-        print('\n''Advanced ingredient label displaying') 
-    else: 
-        print('\n''Advanced ingredient label NOT displaying')
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="broadband-label-BI800"]/div[1]/p[1]').text
-    print("Speed displayed in chart: " + element)
-
-
-    #Gigabit Extra
-    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Gigabit Extra']").click()
-
-    # element = driver.find_element(by=By.XPATH, value='//*[@id="tabpanel-6"]/div/div[2]/div/div[1]')
-    # driver.execute_script("return arguments[0].scrollIntoView();", element)
-
-    timeout = 10
-    try:
-        element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="broadband-label-BI1250"]/div[1]/p[1]'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except:
-        print ('Labels section not showing')
-
-    element = driver.find_element(by=By.XPATH, value="//p[normalize-space()='Gigabit Extra, 1250 Mbps/35 Mbps']").text
-    assert element == 'Gigabit Extra, 1250 Mbps/35 Mbps'
-
-    if "Gigabit Extra, 1250 Mbps/35 Mbps" in element: 
-        print('\n''Gigabit Extra ingredient label displaying') 
-    else: 
-        print('\n''Gigabit Extra ingredient label NOT displaying')
-
-    element = driver.find_element(by=By.XPATH, value='//*[@id="broadband-label-BI1250"]/div[1]/p[1]').text
-    print("Speed displayed in chart: " + element)
-
-#health check verification
-
-    driver.get('https://business.comcast.com/healthcheck/')
-
-    print('\n'"Localized to: "+driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr[3]/td[2]').text) 
-    print ("Session ID: "+driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr[8]/td[2]').text)
+    # Loop through elements, assert href value, and print result
+    for xpath, expected_href in elements.items():
+        element = driver.find_element(By.XPATH, xpath)
+        assert element.get_attribute('href') == expected_href
+        print('\n' + xpath + ' href value matches expected URL')
+        print("CTA URL:", element.get_attribute('href'))
