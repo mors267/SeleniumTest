b3c185a6a060fb954f3026908b8f3b3bd16f30b4
mors267 <morsalin_abedin@comcast.com>
2024-09-18 14:30:20 -0400

    Update test_sampleTest.py

--- test_sampleTest.py
+++ test_sampleTest.py
@@ -1,23 +1,43 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(1920, 1200))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+# and if it doesn't exist, download it automatically,
+# then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+# Add your options as needed    
+options = [
+  # Define window size here
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors"
+]
+
+for option in options:
+    chrome_options.add_argument(option)
 
 
-
-@pytest.fixture(scope="module")
-def browser():
+@pytest.fixture(scope="module")    
+def driver():
     options = Options()
     options.add_argument("--disable-notifications")
     driver = webdriver.Chrome(options=options)
     yield driver
-    driver.quit()
+    driver.quit() 
 
-def test_HomepageLOBTiles(browser):
-    # Load the URL
-    browser.get('https://business.comcast.com/?disablescripts=true')
-    browser.maximize_window()
-    browser.refresh()
+def test_HomepageLOBTiles(driver):
+
+    driver.get('https://business.comcast.com/?disablescripts=true')
+    driver.maximize_window()
 
     # Define elements and their expected href values
     elements = {
@@ -29,21 +49,8 @@ def test_HomepageLOBTiles(browser):
 
     # Loop through elements, assert href value, and print result
     for xpath, expected_href in elements.items():
-        element = browser.find_element(By.XPATH, xpath)
+        element = driver.find_element(By.XPATH, xpath)
         assert element.get_attribute('href') == expected_href
         print('\n' + xpath + ' href value matches expected URL')
         print("CTA URL:", element.get_attribute('href'))
         
-
-
-def test_HomepageHMDSection(browser):
-    # Load the URL
-    browser.get('https://business.comcast.com/?disablescripts=true')
-    browser.maximize_window()
-    browser.refresh()
-
-    # Find and assert href value for HMD section
-    element = browser.find_element(By.CSS_SELECTOR, '.button.button--lg.button-tertiary.button-tertiary--no-border')
-    assert element.get_attribute('href') == 'https://business.comcast.com/learn/solution-finder'
-    print('HMD link available in section')
-    print("CTA URL:", element.get_attribute('href'))
