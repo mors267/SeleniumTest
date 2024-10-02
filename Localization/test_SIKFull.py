35185a2ab782c40dc0641f619b30c0b8c2a61ff2
sadiaorpi <100485787+sadiaorpi@users.noreply.github.com>
2024-10-02 09:40:57 -0400

    Create test_SIKFull.py

---
+++ Localization/test_SIKFull.py
@@ -0,0 +1,148 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(1920, 1200))  
+display.start()
+
+chromedriver_autoinstaller.install()
+
+@pytest.fixture()    
+def quicksetup(): 
+    global driver
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1200")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
+
+    driver.get('https://business.comcast.com/shop/gateway?disablescripts=true')
+    driver.maximize_window()
+    driver.refresh()
+    yield driver
+    driver.quit() 
+
+def test_SIK():  
+    
+    driver = quicksetup
+  
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except TimeoutException:
+        print ('Gateway page did not display')
+    driver.find_element(by=By.XPATH, value="//input[@id='address']").click()
+    driver.find_element(By.ID, "address").send_keys("3201 APOLLO DR OFC CHAMPAIGN IL 61822")
+    time.sleep(5)
+    driver.find_element(by=By.XPATH, value='//*[@id="0"]').click()
+    driver.find_element(by=By.XPATH, value="//button[@type='submit']").click()
+    
+    timeout = 15
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//div[normalize-space()='Advanced']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        print ('Could not localize in SIK enabled location!')
+    time.sleep(3)
+    
+    # click on offer and go in buy flow
+    
+    driver.find_element(by=By.XPATH, value="//div[normalize-space()='Gigabit Extra']").click()
+    driver.find_element(by=By.XPATH, value="//a[@id='9326104226-primary-button']//span[@aria-hidden='true'][normalize-space()='Order Now']").click()
+    
+    timeout = 15
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//span[@class='s-footer-summary-drawer-item-incentive-name fs-14 _extended sik-incentive']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        element = driver.find_element(by=By.XPATH, value="//button[normalize-space()='CONTINUE']")
+        driver.execute_script("return arguments[0].scrollIntoView();", element)
+        time.sleep(3)
+        
+        driver.find_element(by=By.XPATH, value="//button[normalize-space()='CONTINUE']").click()
+        time.sleep(3)
+
+        print ('SIK not displaying in buy flow')
+    
+    time.sleep(3)
+
+    #SIK validation in buy flow
+    element = driver.find_element(by=By.XPATH, value="//h3[@class='cb-reveal-title']")
+    driver.execute_script("return arguments[0].scrollIntoView();", element)
+    time.sleep(3)
+            
+    driver.find_element(by=By.XPATH, value="//button[normalize-space()='CONTINUE']").click()
+    time.sleep(3)
+    timeout = 10
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//span[normalize-space()='I want the Getting Started self-installation kit']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        print ('SIK not showing in installation page')
+    
+    element = driver.find_element(by=By.XPATH, value="//span[normalize-space()='I want the Getting Started self-installation kit']").text
+    assert element == 'I want the Getting Started self-installation kit'
+
+    if "I want the Getting Started self-installation kit" in element: 
+        print('SIK option displaying in installation page') 
+    else: 
+        print('SIK option NOT displaying in installation page')
+    
+    #continue to checkout page
+    driver.find_element(by=By.XPATH, value="//button[normalize-space()='CHECKOUT']").click()
+    time.sleep(3)
+    timeout = 10
+    try:
+        element_present = EC.presence_of_element_located((By.XPATH, "//input[@id='firstName']"))
+        WebDriverWait(driver, timeout).until(element_present)
+    except:
+        print ('Checkout page did not display')
+        
+    driver.find_element(By.ID, "firstName").send_keys("SIK")
+    driver.find_element(By.ID, "lastName").send_keys("Test")
+    driver.find_element(By.ID, "businessName").send_keys("ComcastBusinessTest")
+    driver.find_element(By.ID, "telephoneNumber").send_keys("2672554566")
+    driver.find_element(By.ID, "emailAddress").send_keys("testsik@test.com")
+
+    driver.find_element(by=By.XPATH, value="//button[normalize-space()='CONTINUE']").click()
+    time.sleep(3)
+
+    element = driver.find_element(by=By.XPATH, value="//td[@class='cb-offer-summary-table-item cb-offer-summary-table-highlight--total']")
+    driver.execute_script("return arguments[0].scrollIntoView();", element)
+    time.sleep(3)
+    
+    element = driver.find_element(by=By.XPATH, value="//td[normalize-space()='Self Install']").text
+    assert element == 'Self Install'
+
+    if "Self Install" in element: 
+        print('SIK option displaying in Checkout page') 
+    else: 
+        print('SIK option NOT displaying in Checkout page')
+        
+#health check verification
+
+    driver.get('https://business.comcast.com/healthcheck/')
+
+    print('\n'"Localized to: "+driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr[3]/td[2]').text) 
+    print ("Session ID: "+driver.find_element(by=By.XPATH, value='/html/body/table/tbody/tr[8]/td[2]').text)
